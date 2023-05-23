d07e454098c4e95e38f8b423260125cf78908483
Kartik Gupta <kartik.gupta@intervue.io>
2023-05-23 20:52:44 +0530

    this keyword

--- script.js
+++ script.js
@@ -22,13 +22,23 @@
 // console.log(obj.hobbies)
 // console.log(obj['hobbies'])
 
-greet1('greet2',30)
-function greet1(Name,age){
-    console.log('hello '+Name+' your age is '+age)
-}
+// greet1('greet2',30)
+// function greet1(Name,age){
+//     console.log('hello '+Name+' your age is '+age)
+// }
 
-greet2('greet1',30)
-const greet2 = function(Name,age){
-    console.log('hello '+Name+' your age is '+age)
-}
+// greet2('greet1',30)
+// var greet2 = function(Name,age){
+//     console.log('hello '+Name+' your age is '+age)
+// }
 
+// class Car{
+//     constructor(name,model){
+
+//     }
+//     move(){
+//         console.log('car is moving')
+//     }
+// }
+
+// const car1 = new Car('Audi','A8')
