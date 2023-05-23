a58c42411de9e1123170163e4500a6df1ce63c97
Kartik Gupta <kartik.gupta@intervue.io>
2023-05-23 20:21:04 +0530

    objects

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('hi')
+const car = {
+    name: 'Audi',
+    model: 'A8',
+}
+
+const obj = {
+    name:'John',
+    age: 30,
+    isMarried: false,
+    greet: function(){
+        console.log('hello !!')
+    },
+    address:{
+        city:'Delhi',
+        state:'Delhi',
+        pincode:'110001',
+    },
+    hobbies:['music','sports'],
+    someothercar:car
+}
+
+console.log(obj.hobbies)
+console.log(obj['hobbies'])
