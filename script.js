ea2a1a3c3ec19c18ff389ffcbb24e6ced6315187
Kartik Gupta <kartik.gupta@intervue.io>
2023-05-23 20:42:32 +0530

    function declarations

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
-const car = {
-    name: 'Audi',
-    model: 'A8',
+// const car = {
+//     name: 'Audi',
+//     model: 'A8',
+// }
+
+// const obj = {
+//     name:'John',
+//     age: 30,
+//     isMarried: false,
+//     greet: function(){
+//         console.log('hello !!')
+//     },
+//     address:{
+//         city:'Delhi',
+//         state:'Delhi',
+//         pincode:'110001',
+//     },
+//     hobbies:['music','sports'],
+//     someothercar:car
+// }
+// obj.greet()
+// console.log(obj.hobbies)
+// console.log(obj['hobbies'])
+
+greet2('greet2',30)
+greet2('greet1',30)
+
+function greet1(Name,age){
+    console.log('hello '+Name+' your age is '+age)
 }
 
-const obj = {
-    name:'John',
-    age: 30,
-    isMarried: false,
-    greet: function(){
-        console.log('hello !!')
-    },
-    address:{
-        city:'Delhi',
-        state:'Delhi',
-        pincode:'110001',
-    },
-    hobbies:['music','sports'],
-    someothercar:car
+const greet2 = function(Name,age){
+    console.log('hello '+Name+' your age is '+age)
 }
 
-console.log(obj.hobbies)
-console.log(obj['hobbies'])
