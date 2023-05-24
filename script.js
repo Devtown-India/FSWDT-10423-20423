841db5ac5802d75dbfb9043ffef135c862a34020
Kartik Gupta <kartik.gupta@intervue.io>
2023-05-24 20:42:03 +0530

    more on objects & array methods

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/*
+
 // const car = {
 //     name: 'Audi',
 //     model: 'A8',
@@ -32,13 +34,97 @@
 //     console.log('hello '+Name+' your age is '+age)
 // }
 
-// class Car{
-//     constructor(name,model){
-
-//     }
-//     move(){
-//         console.log('car is moving')
-//     }
+// class Car {
+//   age = 30;
+//   adult = true;
+//   constructor(brand, name) {
+//     console.log(this);
+//     // this.name = name
+//     // this.brand = brand
+//     // this.age = 22
+//   }
+//   greet() {
+//     console.log("hello " + this.name + " your age is " + this.age);
+//   }
 // }
 
-// const car1 = new Car('Audi','A8')
+// function Bike(brand, name) {
+//   this.name = name;
+//   this.brand = brand;
+// }
+
+// const bike1 = new Bike('Ktm','Duke 390');
+// console.log(bike1);
+
+// const obj = {
+//     name:'John',
+//     age: 30,
+// }
+
+// const obj2 = obj
+// obj.name = 'Doe'
+// obj.age = 22
+
+// console.log(obj2)
+
+// // let a = 3
+// // let b = a
+// // a+=25
+
+// // console.log(b)
+// // console.log(a)
+*/
+
+
+// arr.pop()
+
+// console.log(arr.length)
+
+// forEach, map, filter, find , reduce, findIndex
+
+// for(let i = 0;i<arr.length;i++){
+//     console.log(arr[i])
+// }
+// const arr = ['John','Doe','Jane','Jack']
+
+// const callbackFunc = function(potato,apples){
+//     console.log(potato,apples)
+// }
+
+// arr.forEach(callbackFunc);
+
+// for(let i = 0;i<arr.length;i++){
+//     callbackFunc(arr[i],i)
+// }
+
+const arr = [34,21,23]
+const newArr = []
+
+// for(let i = 0;i<arr.length;i++){
+//     newArr.push(arr[i]*19)
+// }
+
+// arr.forEach(function(item){
+//     newArr.push(item*19)
+// })
+
+
+// const newArr2 = arr.map(function(item){
+//     return item*19
+// })
+
+// console.log(newArr2)
+
+// let res = 0
+// for(let i = 0;i<arr.length;i++){
+//     res+=arr[i]
+// }
+// console.log(res)
+
+const reduceFunction = function(previousValue,currentItem){
+    console.log(previousValue,currentItem)
+    return previousValue+currentItem
+}
+
+const res2 = arr.reduce(reduceFunction,0)
+console.log(res2)
