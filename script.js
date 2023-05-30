068aa08dfe0eefb1a2a3a175df3c4e8d69381055
Kartik Gupta <gupta.kartik18kg@gmail.com>
2023-05-30 19:38:15 +0530

    dom methods

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.dir(document)
+// methods for retrieving dom nodes
+// const node = document.getElementById('heading');
+const listItem = document.getElementsByClassName('list-item');
+// const tag = document.getElementsByTagName('li');
+const element = document.querySelectorAll('li');
+console.log(element)
+console.log(listItem)
