d27f7ce961991f9deeae807a54465a5851f34c53
kartik gupta <gupta.kartik18kg@gmail.com>
2023-06-01 19:59:24 +0530

    style using js

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 const heading = document.querySelector('h1')
 const body = document.querySelector('body')
-heading.innerHTML = `
-    <span style="color: red">Hello</span> World!
-`
+const content = document.querySelector('p')
+
+
+// heading.innerHTML = `
+//     <span style="color: red">Hello</span> World!
+// `
+// content.style = `
+//     background:#f1f1f1;
+//     padding:10px;
+//     color:blueviolet;
+// `
+// // console.log(heading.innerHTML)
+// // console.log(heading.textContent)
+let count = 0;
+
+const log = ()=>{
+   heading.innerHTML=count++
+}
+
+
+
+setInterval(()=>{
+var randomColor = Math.floor(Math.random()*16777215).toString(16);
+
+    body.style=`
+        background:#${randomColor};
+        `
+},1000)
