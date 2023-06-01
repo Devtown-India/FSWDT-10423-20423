25175bed98dcb8f95bb8ddcf0ba0e96f4c9496bc
kartik gupta <gupta.kartik18kg@gmail.com>
2023-06-01 20:11:53 +0530

    simple todo

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
 const heading = document.querySelector('h1')
 const body = document.querySelector('body')
 const content = document.querySelector('p')
+// const todos = ['task 1','task 2','task 3']
+const list = document.querySelector('ul')
+const input = document.querySelector('input')
+
+// todos.forEach((todo)=>{
+    // const li = document.createElement('li')
+    // li.innerHTML=todo
+    // console.log(li)
+    // list.appendChild(li)    
+// })
+
+const handleClick = ()=>{
+    const data = input.value
+    if(data===''){
+        return alert('Please enter a value')
+    }
+    console.log(input.value)
+    const li = document.createElement('li')
+    li.innerHTML=input.value
+    console.log(li)
+    list.appendChild(li)   
+}
+
+
+// const li = document.createElement('li')
+// const textInsideli = document.createTextNode('task 4')
+// li.appendChild(textInsideli)
+// console.log(textInsideli)
+// // li.innerHTML='task 4'
+// console.log(li)
 
 
 // heading.innerHTML = `
@@ -13,18 +43,18 @@ const content = document.querySelector('p')
 // `
 // // console.log(heading.innerHTML)
 // // console.log(heading.textContent)
-let count = 0;
+// let count = 0;
 
-const log = ()=>{
-   heading.innerHTML=count++
-}
+// const log = ()=>{
+//    heading.innerHTML=count++
+// }
 
 
 
-setInterval(()=>{
-var randomColor = Math.floor(Math.random()*16777215).toString(16);
+// setInterval(()=>{
+// var randomColor = Math.floor(Math.random()*16777215).toString(16);
 
-    body.style=`
-        background:#${randomColor};
-        `
-},1000)
+//     body.style=`
+//         background:#${randomColor};
+//         `
+// },1000)
