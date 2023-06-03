39a7eb127c9adc458c06f7ca0d02c8ae2ded54b4
kartik gupta <gupta.kartik18kg@gmail.com>
2023-06-03 19:51:50 +0530

    more event listeners

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 const body = document.querySelector('body');
 const textbox = document.querySelector('h1 span');
 const heading = document.querySelector('h1');
+const input = document.querySelector('input');
 
-let count = 0;
-heading.addEventListener('click',()=>{
-    console.log("You clicked")
-    textbox.innerText = ++count;
+window.addEventListener('focus',(e)=>{
+    console.log('tab change detected')
 })
+window.addEventListener('blur',(e)=>{
+    console.log('blur change detected')
+})
+
+window.addEventListener('scroll',(e)=>{
+    console.log('scroll detected',window.scrollY)
+})
+
+
+// let count = 0;
+// heading.addEventListener('click',(event)=>{
+//     console.log(event)
+//     textbox.innerText = ++count;
+// })
+
+// body.addEventListener('mousemove',(event)=>{
+//     console.log({
+//         x: event.clientX,
+//         y: event.clientY
+//     })
+// })
