e17877637b5424af2b5b14ff267bf850250f95b4
kartik gupta <gupta.kartik18kg@gmail.com>
2023-06-03 19:29:33 +0530

    click event listener

--- script.js
+++ script.js
@@ -1,70 +1,9 @@
-const heading = document.querySelector('h1')
-const body = document.querySelector('body')
-const content = document.querySelector('p')
-// const todos = ['task 1','task 2','task 3']
-const list = document.querySelector('ul')
-const input = document.querySelector('input')
+const body = document.querySelector('body');
+const textbox = document.querySelector('h1 span');
+const heading = document.querySelector('h1');
 
-const toggleTheme = ()=>{
-    body.classList.toggle('dark')
-}
-
-// console.log(heading.classList)
-// heading.classList.add('red')
-// heading.classList.add('sm')
-// console.log(heading.classList)
-
-
-// todos.forEach((todo)=>{
-    // const li = document.createElement('li')
-    // li.innerHTML=todo
-    // console.log(li)
-    // list.appendChild(li)    
-// })
-
-// const handleClick = ()=>{
-//     const data = input.value
-//     if(data===''){
-//         return alert('Please enter a value')
-//     }
-//     console.log(input.value)
-//     const li = document.createElement('li')
-//     li.innerHTML=input.value
-//     console.log(li)
-//     list.appendChild(li)   
-// }
-
-
-// const li = document.createElement('li')
-// const textInsideli = document.createTextNode('task 4')
-// li.appendChild(textInsideli)
-// console.log(textInsideli)
-// // li.innerHTML='task 4'
-// console.log(li)
-
-
-// heading.innerHTML = `
-//     <span style="color: red">Hello</span> World!
-// `
-// content.style = `
-//     background:#f1f1f1;
-//     padding:10px;
-//     color:blueviolet;
-// `
-// // console.log(heading.innerHTML)
-// // console.log(heading.textContent)
-// let count = 0;
-
-// const log = ()=>{
-//    heading.innerHTML=count++
-// }
-
-
-
-// setInterval(()=>{
-// var randomColor = Math.floor(Math.random()*16777215).toString(16);
-
-//     body.style=`
-//         background:#${randomColor};
-//         `
-// },1000)
+let count = 0;
+heading.addEventListener('click',()=>{
+    console.log("You clicked")
+    textbox.innerText = ++count;
+})
