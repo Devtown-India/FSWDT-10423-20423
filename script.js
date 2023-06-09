8e840aed35fab42c0b6f8d93dd0610f2666f7298
kartik gupta <gupta.kartik18kg@gmail.com>
2023-06-09 20:22:03 +0530

    promise chaining

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-const response = axios.get('https://jsonplaceholder.typicode.com/todos')
-console.log(response)
+// const response = axios.get('https://jsonplholder.typicode.com/todos')
+// console.log(response)
 
-setTimeout(()=>{
-    console.log(response)
-},1000)
+// response
+// .then((data)=>{console.log(data)})
+// .catch((err)=>{console.log('do something')})
+
+
+/*
+fetch('https://jsonplaceholder.typicode.com/todos')
+.then(response=>{
+  console.log(response)
+  const data = response.json()
+  data.then(newData=>{console.log(newData)})
+})
+*/
+
+const ul = document.createElement('ul')
+document.body.appendChild(ul)
+
+fetch('https://jsonplaceholder.typicode.com/todos')
+.then(response=>response.json())
+.then(data=>{
+  data.forEach(todo=>{
+    const li = document.createElement('li')
+    li.innerHTML = todo.title
+    ul.appendChild(li)
+  })
+})
+.catch(err=>console.log(err))
