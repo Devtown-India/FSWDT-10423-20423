ad3a7711cafb7bbad56b107e5dd20e74ba4c5b58
kartik gupta <gupta.kartik18kg@gmail.com>
2023-06-10 19:29:51 +0530

    async await

--- script.js
+++ script.js
@@ -1,30 +1,26 @@
-// const response = axios.get('https://jsonplholder.typicode.com/todos')
-// console.log(response)
+const ul = document.createElement("ul");
+document.body.appendChild(ul);
 
-// response
-// .then((data)=>{console.log(data)})
-// .catch((err)=>{console.log('do something')})
+// fetch('https://jsonplaceholder.typicode.com/todos')
+// .then(response=>response.json())
+// .then(data=>{
+//   data.forEach(todo=>{
+//     const li = document.createElement('li')
+//     li.innerHTML = todo.title
+//     ul.appendChild(li)
+//   })
+// })
 
+const fetchData = async () => {
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const data = await res.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }finally{
+    console.log('finally')
+  }
+};
 
-/*
-fetch('https://jsonplaceholder.typicode.com/todos')
-.then(response=>{
-  console.log(response)
-  const data = response.json()
-  data.then(newData=>{console.log(newData)})
-})
-*/
-
-const ul = document.createElement('ul')
-document.body.appendChild(ul)
-
-fetch('https://jsonplaceholder.typicode.com/todos')
-.then(response=>response.json())
-.then(data=>{
-  data.forEach(todo=>{
-    const li = document.createElement('li')
-    li.innerHTML = todo.title
-    ul.appendChild(li)
-  })
-})
-.catch(err=>console.log(err))
+fetchData();
