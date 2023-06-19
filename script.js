70257724b26227443d69eb547c20038c061fbe6a
kartik gupta <gupta.kartik18kg@gmail.com>
2023-06-19 19:49:20 +0530

    nodejs

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-const message = document.querySelector('.message')
-const button = document.querySelector('button')
-const input = document.querySelector('input')
-
-const checkPallindrome = ()=>{
-  const word = input.value
-  const reverseWord = word.split('').reverse().join('')
-  if(word === reverseWord){
-    message.innerText = `${word} is a pallindrome`
-  }else{
-    message.innerText = `${word} is not a pallindrome`
-  }
-}
-
-button.addEventListener('click', checkPallindrome)
+const a= 10
+a++
+console.log(a)
