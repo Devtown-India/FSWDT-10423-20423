575bc33097a593a6d9956702b9f0e33800306e7b
kartik gupta <gupta.kartik18kg@gmail.com>
2023-06-21 20:02:58 +0530

    fs module

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log(require('./math.js'))
+const fs = require('fs');
+
+const data = "Hello world"
+
+fs.writeFile('test.txt',data,(err)=>{
+    if(err) throw err;
+    console.log('File written to...');
+})
+
+console.log('next')
--- math.js
+++
@@ -1,9 +0,0 @@
-const add = (...rest)=>rest.reduce((a,b)=>a+b,0);
-
-const multiply = (...rest)=>rest.reduce((a,b)=>a*b,1);
-
-module.exports = {
-    add,
-    multiply
-}
-
