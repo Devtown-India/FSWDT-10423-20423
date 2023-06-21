25ee1ed713c280b35e46076fc1166da3ee080e64
kartik gupta <gupta.kartik18kg@gmail.com>
2023-06-21 20:06:46 +0530

    fs module

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const fs = require('fs');
 
-const data = "Hello world"
+// const data = "Hello world"
 
-fs.writeFile('test.txt',data,(err)=>{
+// fs.writeFile('test.txt',data,(err)=>{
+//     if(err) throw err;
+//     console.log('File written to...');
+// })
+
+// console.log('next')
+
+const data = fs.readFileSync('test.txt',{encoding:'utf8'})
+fs.readFile('test.txt',{encoding:'utf8'},(err,data)=>{
     if(err) throw err;
-    console.log('File written to...');
+    console.log(data,'async')
 })
-
-console.log('next')
+console.log(data,'sync')
