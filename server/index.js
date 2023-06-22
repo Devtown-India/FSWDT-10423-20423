41923ca003fdc47517a554f420cdb2f391401680
kartik gupta <gupta.kartik18kg@gmail.com>
2023-06-22 19:17:10 +0530

    developer dependencies

---
+++ server/index.js
@@ -0,0 +1,19 @@
+// const { v4: uuidv4 } = require('uuid')
+
+// const id = uuidv4()
+
+// console.log(fetch)
+const axios = require("axios");
+
+// const fetchData = async () => {
+//   try {
+//     const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+//     console.log(res.data);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+console.log('hi')
+
+// fetchData();
