04e5657abfcfea212fe70022698b54be5bc55d75
kartik gupta <gupta.kartik18kg@gmail.com>
2023-06-22 18:29:50 +0530

    boilerplate

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 const fs = require('fs');
 
-// const data = "Hello world"
+// // const data = "Hello world"
 
-// fs.writeFile('test.txt',data,(err)=>{
+// // fs.writeFile('test.txt',data,(err)=>{
+// //     if(err) throw err;
+// //     console.log('File written to...');
+// // })
+
+// // console.log('next')
+
+// const data = fs.readFileSync('test.txt',{encoding:'utf8'})
+// fs.readFile('test.txt',{encoding:'utf8'},(err,data)=>{
 //     if(err) throw err;
-//     console.log('File written to...');
+//     console.log(data,'async')
 // })
+// console.log(data,'sync')
 
-// console.log('next')
 
-const data = fs.readFileSync('test.txt',{encoding:'utf8'})
-fs.readFile('test.txt',{encoding:'utf8'},(err,data)=>{
-    if(err) throw err;
-    console.log(data,'async')
-})
-console.log(data,'sync')
+const createFile = (data,name)=>{
+    try{
+        fs.writeFileSync(name,data)
+        console.log('File written to...')
+    }catch(err){
+        console.log(err)
+    }
+}
+
+createFile('Hello  again world','tes2t.txt')
