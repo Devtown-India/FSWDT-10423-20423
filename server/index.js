ccd126dbb0a2ef77d338d8ecfadf63749ba2dca8
kartik gupta <gupta.kartik18kg@gmail.com>
2023-06-23 19:44:09 +0530

    express server

--- server/index.js
+++ server/index.js
@@ -1,19 +1,9 @@
-// const { v4: uuidv4 } = require('uuid')
+const express = require('express')
 
-// const id = uuidv4()
+const PORT = 8080
+// expres instance
+const app = express()
 
-// console.log(fetch)
-const axios = require("axios");
-
-// const fetchData = async () => {
-//   try {
-//     const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
-//     console.log(res.data);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
-console.log('hi')
-
-// fetchData();
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
