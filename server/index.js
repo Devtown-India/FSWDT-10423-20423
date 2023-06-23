6e05430f516300adc7bb29441212da048e0976ac
kartik gupta <gupta.kartik18kg@gmail.com>
2023-06-23 20:02:45 +0530

    http requests

--- server/index.js
+++ server/index.js
@@ -4,6 +4,19 @@ const PORT = 8080
 // expres instance
 const app = express()
 
+app.get('/',(req,res)=>{
+    // console.log(req)
+    const {headers,method,url,path,query} = req
+    console.log({
+        headers,
+        method,
+        url,
+        path,
+        query
+    })
+    res.send('Hello World')
+})
+
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`)
 })
