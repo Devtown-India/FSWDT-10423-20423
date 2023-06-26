886710d02c82fc3a97bee1037a9dbe1f2eeaf45a
kartik gupta <gupta.kartik18kg@gmail.com>
2023-06-26 19:32:34 +0530

    body parser middleware

--- server/index.js
+++ server/index.js
@@ -1,46 +1,55 @@
-const express = require('express')
-const fs = require('fs/promises')
+const express = require("express");
+const fs = require("fs/promises");
 
-const PORT = 8080
+const PORT = 8080;
 // expres instance
-const app = express()
+const app = express();
 
-const logger = async(req,res,next)=>{
-    // do something
-    const {headers,method,url,path,query} = req
-    console.log({
-        headers,
-        method,
-        url,
-        path,
-        query
-    })
-next()
-}
+const logger = async (req, res, next) => {
+  // do something
+  const { headers, method, url, path, query } = req;
+  console.log({
+    headers,
+    method,
+    url,
+    path,
+    query,
+  });
+  next();
+};
 
-app.use(logger)
-
-
-app.get('/',(req,res)=>{
-    // console.log(req)
-   
-    res.status(200).send('SErver is running')
-})
+app.use(express.json());
+// app.use(logger);
 
 
-app.get('/todos',async (req,res)=>{
-    try {
-        const {count=10} = req.query
-        const todos = await fs.readFile('./db.json','utf-8')
-        const parsedTodos = JSON.parse(todos)
-        res.status(200).json(parsedTodos.slice(0,count))
-    } catch (error) {
-        console.log(error)
-        res.status(500).json(error)
-    }
-})
+app.get("/", (req, res) => {
+  // console.log(req)
 
+  res.status(200).send("SErver is running");
+});
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`)
-})
+app.post("/signup", async (req, res) => {
+  try {
+    const {email, password} = req.body;
+    return res.status(200).json({email,password})
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
+
+app.get("/todos", async (req, res) => {
+  try {
+    const { count = 10 } = req.query;
+    const todos = await fs.readFile("./db.json", "utf-8");
+    const parsedTodos = JSON.parse(todos);
+    res.status(200).json(parsedTodos.slice(0, count));
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
