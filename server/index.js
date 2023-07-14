cc30678bb8dd008e36d5846dfd7f489dca24ffc8
kartik gupta <gupta.kartik18kg@gmail.com>
2023-07-14 20:06:57 +0530

    esm

--- server/index.js
+++ server/index.js
@@ -1,18 +1,21 @@
-const express = require("express");
-const authRoutes = require("./routes/auth");
-const todoRoutes = require("./routes/todo");
-const pageRoutes = require("./routes/pages");
+// const express = require("express");
+// const authRoutes = require("./routes/auth");
+// const todoRoutes = require("./routes/todo");
+// const pageRoutes = require("./routes/pages");
 
-const PORT = 8080;
-// expres instance
-const app = express();
-app.use(express.json());
+// const PORT = 8080;
+// // expres instance
+// const app = express();
+// app.use(express.json());
 
-app.use('/api/auth',authRoutes)
-app.use('/api/todos',todoRoutes)
+// app.use('/api/auth',authRoutes)
+// app.use('/api/todos',todoRoutes)
 
-app.use('/',pageRoutes)
+// app.use('/',pageRoutes)
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+import {greet} from './a.js'
+
+greet()
