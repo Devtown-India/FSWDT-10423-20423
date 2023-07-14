14fdd6296243c59c3db3df525fb6c31270f91999
kartik gupta <gupta.kartik18kg@gmail.com>
2023-07-14 20:09:27 +0530

    esm syntax for import and export

--- server/a.js
+++ server/a.js
@@ -1,1 +1,5 @@
 export const greet = ()=>console.log('hi')
+
+const age = 22
+
+export default age
--- server/index.js
+++ server/index.js
@@ -16,6 +16,6 @@
 // app.listen(PORT, () => {
 //   console.log(`Server is running on port ${PORT}`);
 // });
-import {greet} from './a.js'
-
-greet()
+import defaultthing, {greet} from './a.js'
+console.log(greet)
+// greet()
