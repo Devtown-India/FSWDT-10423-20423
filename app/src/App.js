06e9629c690fe3382c68d08fce3833508b073b45
kartik gupta <gupta.kartik18kg@gmail.com>
2023-08-14 19:09:09 +0530

    jsx

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,18 @@
 const App = () => {
+
+  const tasks = ['task 1', 'task 2', 'task 3']
+  const age = 10
+
   return ( 
-    <h1>Hello from react</h1>
+    <div>
+      <h1>Task List</h1>
+      {age}
+      {
+        tasks.map((task, index) => {
+          return <p>{task}</p>
+        })
+      }
+    </div>
    );
 }
  
