4345b09bd0b4b565db671a396df54b730225a94e
kartik gupta <gupta.kartik18kg@gmail.com>
2023-08-15 19:54:18 +0530

    state in react

--- app/src/components/Content.jsx
+++ app/src/components/Content.jsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
+
 const Content = () => {
-  let count = 0;
+  //   let count = 0;
+  let [count, setCount] = useState(0);
+  //! state is immutable
 
   const increment = (e) => {
+    setCount((prev) => prev + 1);
     console.log(count);
   };
 
