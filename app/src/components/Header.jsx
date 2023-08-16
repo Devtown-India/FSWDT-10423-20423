26880edd4f02461688f211b0573a0811fb4f8984
kartik gupta <gupta.kartik18kg@gmail.com>
2023-08-16 20:01:16 +0530

    props

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -2,10 +2,11 @@ import { useState } from "react";
 
 const Header = ({ handleAdd }) => {
   const [inputText, setInputText] = useState("");
+  const handleChange = (e) => setInputText(e.target.value);
 
   return (
     <div>
-      <input onChange={(e) => setInputText(e.target.value)} type="text" />
+      <input onChange={handleChange} type="text" />
       <button onClick={() => handleAdd(inputText)}>Add</button>
     </div>
   );
