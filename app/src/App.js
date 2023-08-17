96d47b127b9248d41c206d6aa549892dc02b2768
kartik gupta <gupta.kartik18kg@gmail.com>
2023-08-17 22:34:08 +0530

    react router

--- app/src/App.js
+++ app/src/App.js
@@ -5,17 +5,8 @@ import Home from "./components/Home";
 import {Routes, Route} from 'react-router-dom';
 
 const App = () => {
-  const [route, setRoute] = useState(window.location.pathname);
-  // console.log(window.location.pathname)
   return (
     <div>
-      {/* {route === "/contact" ? (
-        <Contact setRoute={setRoute} />
-      ) : route === "/about" ? (
-        <About setRoute={setRoute} />
-      ) : (
-        <Home setRoute={setRoute} />
-      )} */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
