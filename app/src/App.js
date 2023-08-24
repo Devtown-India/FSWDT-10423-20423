212a13210c9a0d1f0c070b207535b4dbb99f2788
kartik gupta <gupta.kartik18kg@gmail.com>
2023-08-24 20:07:31 +0530

    boilerplate for full stack todo app

--- app/src/App.js
+++ app/src/App.js
@@ -1,19 +1,13 @@
 import { useState } from "react";
 import {Routes, Route} from 'react-router-dom';
-import Footer from "./layout/Footer";
-import Navbar from "./layout/Navbar";
-import Quiz from "./components/Quiz";
-import Result from "./components/Result";
+import Login from "./components/Login";
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
       <Routes>
-        <Route path="/" element={<Quiz/>}/>
-        <Route path="/result" element={<Result/>}/>
+        <Route path="/login" element={<Login/>}/>
       </Routes>
-      <Footer/>
     </div>
   );
 };
