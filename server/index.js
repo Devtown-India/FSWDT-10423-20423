56b543402c7dabb2613bfe535bca44156d0d2b87
kartik gupta <gupta.kartik18kg@gmail.com>
2023-08-25 20:43:17 +0530

    signup route

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const authRoutes = require("./routes/auth");
 const todoRoutes = require("./routes/todo");
 const pageRoutes = require("./routes/pages");
+const cors = require("cors");
 
 const PORT = 8080;
 // expres instance
 const app = express();
+app.use(cors())
 app.use(express.json());
 
 app.use('/api/auth',authRoutes)
