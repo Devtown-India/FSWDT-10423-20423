db4952c3657e87603af4706cd0c1c055826be255
kartik gupta <gupta.kartik18kg@gmail.com>
2023-08-29 20:35:54 +0530

    context

--- app/src/App.js
+++ app/src/App.js
@@ -1,16 +1,16 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Todo from "./components/Todos";
 import LayerOne from "./components/LayerOne";
-import { TodoProvider } from "./contexts/TodoContext";
+import { TodoContext, TodoProvider } from "./contexts/TodoContext";
 
 const Header = ({ children }) => {
-  console.log(children)
+  const context = useContext(TodoContext)
+  console.log(context)
   return <h1 className="text-red-500">
     data
-    {children}
     </h1>;
 };
 
@@ -20,6 +20,8 @@ const App = () => {
     <div>
       <TodoProvider>
         <LayerOne />
+      <Header/>
+
       </TodoProvider>
     </div>
   );
