f946fd1a6013ea536abe6543e7a6648fc3f49108
kartik gupta <gupta.kartik18kg@gmail.com>
2023-08-29 21:08:06 +0530

    add todo

--- app/src/components/AddTodo.jsx
+++ app/src/components/AddTodo.jsx
@@ -1,11 +1,23 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { TodoContext } from "../contexts/TodoContext";
 
 const AddTodo = () => {
   const [todo, setTodo] = useState("");
+  const { setTodos } = useContext(TodoContext);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("form submitted", todo);
+    setTodos((prevTodos) => {
+      return [
+        ...prevTodos,
+        {
+          id: (Math.random() * 1000).toFixed(0),
+          title: todo,
+          completed: false,
+        },
+      ];
+    });
   };
   return (
     <form onSubmit={handleSubmit}>
