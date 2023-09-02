d7d053202b122b46d09fb3b0c292c95bb70aba5f
kartik gupta <gupta.kartik18kg@gmail.com>
2023-09-02 20:36:26 +0530

    keys in react

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,24 @@
 import Header from "./components/Header";
 import Todos from './components/Todos.jsx'
+import {useState} from 'react'
+
+const randomId = () => {
+  return Math.floor(Math.random() * 100000);
+};
 
 const App = () => {
+  const [items,setItems] = useState([])
+  const handleAdd = ()=>{
+    setItems([randomId(),...items])
+  }
   return ( 
-      <div>
-        <Header/>
-        <Todos/>
+      <div >
+        <button onClick={handleAdd} >Add</button>
+        {
+          items.map((item,index) => {
+            return <input key={item} />
+          })
+        }
       </div>
    );
 }
