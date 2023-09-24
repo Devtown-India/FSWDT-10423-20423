3b08cc08d85d1b8575baee6f84d84f37bfb868fe
kartik gupta <gupta.kartik18kg@gmail.com>
2023-09-24 18:59:29 +0530

    login page

--- client/src/redux/actions/auth.js
+++ client/src/redux/actions/auth.js
@@ -1,20 +1,17 @@
 import customAxios from "../../utils/axios";
 
-export const loginUser = (data) => {
-  return async (dispatch) => {
-    try {
-      const res = await customAxios.post("/auth/login", data);
-      const { token, user } = res.data;
-      localStorage.setItem("token", token);
-      dispatch({
-        type: "LOGIN",
-        payload: {
-          token,
-          user,
-        },
-      })
-    } catch (error) {
-      console.log(error);
-    }
+export const loginUser = async (data) => {
+  try {
+    const res = await customAxios.post("/auth/login", data);
+    const { token, user } = res.data;
+    return {
+      type: "LOGIN",
+      payload: {
+        token,
+        user,
+      },
+    };
+  } catch (error) {
+    console.log(error);
   }
 };
